126192164024d8c95c4a0eb00a37f5da5a2e8d8e
hafed03 <hafidz.skatel17@gmail.com>
2021-05-05 10:28:16 +0700

    komentar

--- application/views/Pelajar/Pelajar_View.php
+++ application/views/Pelajar/Pelajar_View.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
-<html>
-<head>
+<!--
+	Pendahuluan: halaman ini merupakan tampilan awal ketika pelajar login 
+	sebagai pelajar
+	rujukan DPPL ada pada halaman 29
+-->
+<!-- on going progress-->
+<html><!--menyatakan penggunaan html versi 5-->
+<head><!-- head html-->
+	<!-- pemanggilan bootstrap dari internet-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!--menyisipkan folder style.css dari folder lokal-->
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title> Pelajar </title>
-</head>
-<body>
-	<nav class="navbar navbar-light bg-light justify-content-between">
-	  	<a class="navbar-brand"> Logo </a>
-	  	<button class="btn btn-outline-warning my-2 my-sm-0" type="submit" onclick="window.location.href='<?php echo base_url().'index.php/login/logout'?>'"> Logout </button>
-	</nav>
+	<title> Pelajar </title><!--title html untuk nama halaman-->
+</head><!--penutupan head html-->
+<body><!--body html-->
+	<?php //menyatakan penggunaan php
+		$this->load->view('template/navbar');//memanggil navbar dari folder view untuk bagian atas halaman
+	?>
 
-	<h1>Halaman Pelajar</h1>
+	<h1>Halaman Pelajar</h1><!--heading no.1 html -->
 
-</body>
-</html>
+</body><!--penutupan body html-->
+</html><!--penutupan penggunaan html versi 5-->
