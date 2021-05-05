9cdf30bbfb4271b8904dbca6a568b427e4dc37eb
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2021-05-05 09:53:42 +0700

    commentary

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,42 +1,55 @@
+<!-- Halaman ini menyatakan bagian depan dari halaman admin.
+Saat admin melakukan login, halaman inilah yang akan ditampilkan kepada admin untuk pertama kali.
+Rujukan DPPL source code ini adalah DPPL yang bisa diakses di https://github.com/ndrawa/impal/blob/master/DPPL.pdf pada halaman 31.-->
+
+<!-- Pada halaman ini hanya menghubungkan antar halaman dari menu utama admin ke masing-masing menu admin.
+Halaman yang terhubung adalah halaman create matakuliah, view matakuliah, edit matakuliah, delete matakuliah, view materi, dan view pengajar.
+Karena kami menggunakan framework Codeigniter, jadi pada halaman view tidak terdapat method-method yang berfungsi untuk melakukan pengolahan data.
+Semua pengolahan data dilakukan pada model dan controller. -->
+
+<!-- On-going progress. -->
+
 <!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <!-- Menyatakan penggunaan html versi 5 -->
 <html>
 <!-- head html -->
 <head>
-  <!-- memanggil bootstrap dari internet -->
+  <!-- Memanggil bootstrap dari internet -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <!-- menyisipkan folder style.css dari folder lokal -->
+  <!-- Menyisipkan folder style.css dari folder lokal -->
 	<link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>"/>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <!-- Memberikan title untuk nama halaman. Menyatakan ini halaman milik siapa -->
 	<title> Admin </title>
 </head>
 <body>
     <?php 
+      // Memanggil navbar untuk keperluan bagian atas halaman
 			$this->load->view('template/navbar');
 		?>
+    <!-- Membuat container untuk hyperlink/tombol yang ada -->
     <div class="container">
-    <div class="row">
-      <div class="col">
-      <p class="h5 text-center mb-4">Menu Admin</p>
-        <form class="form-control" action="<?= site_url('login/auth') ?>" method="post" id="createmateri">
+      <div class="row">
+        <div class="col">
+          <!-- Menyatakan bahwa ini menu admin -->
+          <p class="h5 text-center mb-4">Menu Admin</p>
+          <!-- div untuk bagian link -->
           <div class="text-center">
-            <!-- <button class="btn btn-unique" onclick="window.location.href='<?= site_url('admin_controller/create_matakuliah/'); ?>'">Crate MataKuliah</button> -->
+            <!-- Tombol akan membuka ke link halaman yang dituju -->
             <a href="<?= site_url('admin_controller/create_matakuliah/'); ?>">Create MataKuliah   </a>
             <a href="<?= site_url('admin_controller/view_matakuliah/'); ?>">View MataKuliah   </a>
             <a href="<?= site_url('admin_controller/edit_matakuliah/'); ?>">Edit MataKuliah   </a>
             <a href="<?= site_url('admin_controller/delete_matakuliah/'); ?>">Delete MataKuliah   </a>
+            <!-- <button class="btn btn-unique" onclick="window.location.href='<?= site_url('admin_controller/create_matakuliah/'); ?>'">Crate MataKuliah</button> -->
+            <!-- <button class="btn btn-unique" type="submit">Create MataKuliah</button>
             <button class="btn btn-unique" type="submit">Edit MataKuliah</button>
             <button class="btn btn-unique" type="submit">View MataKuliah</button>
             <button class="btn btn-unique" type="submit">Delete MataKuliah</button> -->
           </div>
-        </form>
+        </div>
       </div>
-    </div>
     </div>    
+<!-- Menutup body -->
 </body>
+<!-- Menutup file html -->
 </html>
