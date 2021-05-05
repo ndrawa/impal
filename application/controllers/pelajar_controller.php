b62c58cebbca3511e557e8c79e59aebc6b784b7d
priyogaaditya <64309007+priyogaaditya@users.noreply.github.com>
2021-05-05 09:18:58 +0700

    Update pelajar_controller.php
    modul 10

--- application/controllers/pelajar_controller.php
+++ application/controllers/pelajar_controller.php
@@ -1,18 +1,18 @@
 <?php
  class pelajar extends CI_Controller {
  	public function __construct() {
- 		parent::__construct();
- 		$this->load->model('pelajar_model');
+ 		parent::__construct(); //Deklarasi method konstruk untuk kelas pelajar
+ 		$this->load->model('pelajar_model'); //memuat model dari pelajar
  	}
  	
  	public function index() {
-		$data[‘pelajar’] = $this->pelajar_model->get_all_pelajar();
-		$this->load->view('pelajar/V_KelolaPelajar', $data);  
+		$data[‘pelajar’] = $this->pelajar_model->get_all_pelajar();//menyimpan indeks pelajar didalam list
+		$this->load->view('pelajar/Pelajar_View', $data); //memuat halaman pelajar dengan data yang sudah tersmipan di dalam list
  	}
 
 	public function get_pelajar_data() {
-		$username = $this->sesison->userdata('session_username');
-		$result = $this->pasien_model->get_by_username($username);
-    	echo json_encode($result);
+		$username = $this->sesison->userdata('session_username'); //mengambil nama pelajar dari session username
+		$result = $this->Pelajar_Model->get_by_username($username); //mengambil data pelajar dari database berdasarkan $username
+    	echo json_encode($result); //mengubah tampilan json ke tampilan yang mudah dibaca
 	}
 }
