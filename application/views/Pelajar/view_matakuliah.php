bc39b66c9e17e39230844bb4f347ff6cce250368
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2021-06-08 23:26:16 +0700

    Create view_matakuliah.php

---
+++ application/views/Pelajar/view_matakuliah.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title> Daftar Mata Kuliah - Pelajar</title>
+</head>
+<body>
+
+	<?php 
+		$this->load->view('template/navbar');
+  		$this->load->view('template/back');
+	?>
+
+	<h1> Lihat Mata Kuliah </h1>
+
+</body>
+</html>
