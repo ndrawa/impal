4c91a9fd572fda4865de24c0089c2eb737fe2893
Indra <indrawahyudi2710@gmail.com>
2021-06-16 09:25:56 +0800

    update logo navbar

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,4 +1,4 @@
 <nav class="navbar navbar-light bg-light justify-content-between">
-    <a class="navbar-brand"> Logo </a>
+    <a class="navbar-brand" href="<?= site_url('/login/auth');?>"> Logo </a>
     <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" onclick="window.location.href='<?php echo base_url().'index.php/login/logout'?>'"> Logout </button>
 </nav>
