17fb558de034a2e971d0ae6be04734dc80ab2a9e
Indra <indrawahyudi2710@gmail.com>
2021-06-30 00:25:02 +0800

    update view

--- application/views/Pengajar/Pengajar_View.php
+++ application/views/Pengajar/Pengajar_View.php
@@ -11,7 +11,8 @@
 		$this->load->view('template/navbar');
 	?>
 	<div style="background-color: #e8e8e8;">
-		<h3 style="margin:10px;">Selamat datang, Pak <?= $this->session->userdata('session_nama');?> </h3>
+		<h4 style="margin-left: 50px;">Selamat datang, Pak <?= $this->session->userdata('session_nama');?> </h3>
+		<p style="margin-left: 50px;"> Silahkan pilih tombol untuk memulai action. </p>
 	</div>
 	<div align="center" style="margin: 50px auto;">
 		<a href="<?= site_url('pengajar_controller/input_materi/'); ?>" class="btn btn-primary">Input Materi </a>
